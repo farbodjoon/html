cc0e3cc9e683449217e08643096514359064d595
farbod <ebrahimidoctor@yahoo.com>
2022-07-01 20:19:28 +0430

    mouse boutton

--- myScripts.js
+++ myScripts.js
@@ -1,54 +1,53 @@
-// 5 تا شی پیدا کن  همه ویژگی هاشو در بیار همه ویژگی ها رو توی تگ های وختلف چاپ کن
-function main() {
+function days(day) {
+  if (day == 0) {
+    document.getElementById('day_p_tag').innerHTML = "1"
+  }
+  else if (day == 1) {
+    document.getElementById('out').innerHTML = "2"
+  }
+  else if
+    (day == 2) {
+    document.getElementById('out').innerHTML = "3"
+  }
+  else if
+    (day == 3) {
+    document.getElementById('out').innerHTML = "4"
+  }
+  else if
+    (day == 4) {
+    document.getElementById('out').innerHTML = "5"
+  }
 
-    let person={name:'class7',height:100,_Number:15,color:'red',usechair:true,userwindow:true,usemarkerbord:false};
-    print(person);
+  else if
+    (day == 5) {
+    document.getElementById('out').innerHTML = "6"
+  }
+  else
+    document.getElementById('out').innerHTML = "7"
 
-}
-function main2(){
-    let rayane={name:'laptab',height:200,_Number:10,color:'gray',userlight:true,userkebord:true,userpaper:false};
-    print(rayane);
-
-}
-
-function main3() {
-    let move={name:'car',wight:100,color:'colory',userwindow:true,userdoor:true,_tireNumber:4};
-    print(move);
-
-} 
-
-function main4(){
-    let light={name:'lamp',height:50,color:'Red',_Number:12,userelectrice:true,userglass:true,USERMETAL:false};
-    print(light);
-
-}
-
-function main5(){
-    let air={name:'hairderayer',height:90,color:'black',_Numberpart:3,USERMETAL:true,userWATER:false};
-    print(air);
-}
-
-function printperson(x) {
-document.getElementById('book').innerHTML=x.color;
-document.getElementById('book').innerHTML=x.name;
+  out
 }
 
-function printrayane(y){
-document.getElementById('book').innerHTML=x.color;
-document.getElementById('book').innerHTML=x.name;
+function daysOut() {
+  document.getElementById("day_p_tag").innerHTML = "هیچ روزی کلیک نشده"
 }
 
-function printmove(t){
-document.getElementById('book').innerHTML=y.color;
-document.getElementById('book').innerHTML=y.name;
+function mounth(year) {
+  if (year == 0) { document.getElementById('year').innerHTML = "1" }
+  else if (year == 1) { document.getElementById('year').innerHTML = "فروردین" }
+  else if (year == 2) { document.getElementById('year').innerHTML = "3" }
+  else if (year == 3) { document.getElementById('year').innerHTML = "4" }
+  else if (year == 4) { document.getElementById('year').innerHTML = "5" }
+  else if (year == 5) { document.getElementById('year').innerHTML = "6" }
+  else if (year == 6) { document.getElementById('year').innerHTML = "7" }
+  else if (year == 7) { document.getElementById('year').innerHTML = "8" }
+  else if (year == 8) { document.getElementById('year').innerHTML = "9" }
+  else if (year == 9) { document.getElementById('year').innerHTML = "10" }
+  else if (year == 10) { document.getElementById('year').innerHTML = "11" }
+  else { document.getElementById('mounth') }
+
 }
 
-function printlight(p) {
-document.getElementById('book').innerHTML=p.color;
-document.getElementById('book').innerHTML=p.name;
-}
-
-function printair(f) {
-document.getElementById('book').innerHTML=f.color;
-document.getElementById('book').innerHTML=f.name;
+function fullName(name, lastname) {
+  document.getElementById("note").innerHTML=name+lastname+""
 }
